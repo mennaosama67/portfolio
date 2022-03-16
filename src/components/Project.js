538a3d3a29844a803fb41848aed6672081126a88
mennaosama67 <menna7868@gmail.com>
2022-03-16 18:38:39 +0200

    updating portfolio component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import PortfolioBtn from './PortfolioBtn';
-function Project({imageSrc,codeUrl,demoUrl}) {
+function Project({imageSrc,codeUrl,demoUrl,projectName}) {
   return  ( 
     <div className="project col-lg-4 col-md-12 col-sm-12" data-aos="zoom-in-up">
   <img src={imageSrc} alt="project" />
   <div className="project-caption">
+  <h3>{projectName}</h3>
    <a href={demoUrl} target="_blank" rel="noopener noreferrer"><PortfolioBtn ><i className="far fa-eye" title = "View Demo"></i></PortfolioBtn></a>
    <a href={codeUrl} target="_blank" rel="noopener noreferrer"><PortfolioBtn ><i className="fas fa-code" title = "View Code"></i></PortfolioBtn></a>
   </div>
--- src/layout/Portfolio.js
+++ src/layout/Portfolio.js
@@ -4,6 +4,8 @@ import pompeo from "../images/pompeo.jpg";
 import scienc from '../images/sceinc.jpg'
 import infinity from '../images/infinity.jpg'
 import travel from '../images/travel-site.png'
+import netflix from '../images/netflex-img.PNG'
+import ecommerce from '../images/e-commerce-img.PNG'
 import MainHeader from './../components/MainHeader';
 import { Element } from 'react-scroll'
 
@@ -15,13 +17,17 @@ function Portfolio() {
       <MainHeader >Portfolio</MainHeader>
       <h1 className="about-header">See My Latest Projects.</h1>
       <div className="row">
-      <Project imageSrc={pompeo} codeUrl="https://github.com/mennaosama67/pompeo" demoUrl="https://pompeo-react.netlify.app/"/>
-      <Project imageSrc={scienc} codeUrl="https://github.com/mennaosama67/scenic" demoUrl="https://scenic-site.netlify.app/"/>
-      <Project imageSrc={infinity} codeUrl="https://github.com/mennaosama67/infinity" demoUrl="https://infinity-site.netlify.app/"/>
+      <Project imageSrc={pompeo} codeUrl="https://github.com/mennaosama67/pompeo" demoUrl="https://pompeo-react.netlify.app/" projectName="Pompeo e-commerce"/>
+      <Project imageSrc={scienc} codeUrl="https://github.com/mennaosama67/scenic" demoUrl="https://scenic-site.netlify.app/" projectName="Scenic landing page"/>
+      <Project imageSrc={infinity} codeUrl="https://github.com/mennaosama67/infinity" demoUrl="https://infinity-site.netlify.app/" projectName=" infinity landing page"/>
     
       </div>
       <div className="row">
-      <Project imageSrc={travel} codeUrl="https://github.com/mennaosama67/travel-website" demoUrl="https://mennaosama67.github.io/travel-website/"/>
+      <Project imageSrc={travel} codeUrl="https://github.com/mennaosama67/travel-website" demoUrl="https://mennaosama67.github.io/travel-website/" projectName="Travel"/>
+      <Project imageSrc={netflix} codeUrl="https://github.com/mennaosama67/netflix-clone" demoUrl="https://netflix-clone67.netlify.app/" projectName="Netflix"/>
+      <Project imageSrc={ecommerce} codeUrl="https://github.com/mennaosama67/e-commerce" demoUrl="https://mennaosama67.github.io/e-commerce/" projectName="Rymo e-commerce"/>
+
+
       </div>
    
     </div>
